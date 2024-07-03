d8e1c1f7b0f4fbdd43354902fe1d112176aa379f
Sai Teja <149715173+SaiTeja250802@users.noreply.github.com>
2024-07-03 12:51:37 +0530

    Update Helmet_detection.py

--- Helmet_detection.py
+++ Helmet_detection.py
@@ -7,8 +7,7 @@ model_path = "best.pt"
 model = YOLO(model_path)
 
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)  # Use 0 for built-in webcam, 1 for external
+cap = cv2.VideoCapture(0)  
 
 while True:
     ret, frame = cap.read()
@@ -16,19 +15,14 @@ while True:
         print("Failed to grab frame")
         break
 
-    # Perform detection on the current frame
     results = model(frame)
 
-    # Annotate the frame with detection results
     annotated_frame = results[0].plot()
-
-    # Display the resulting frame
+  
     cv2.imshow('YOLO Live Detection', annotated_frame)
-
-    # Press 'q' to exit the loop
+   
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the webcam and close windows
 cap.release()
 cv2.destroyAllWindows()
